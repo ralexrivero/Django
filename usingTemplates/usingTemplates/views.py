5857e8f665266ac02a81b73a1a74310d8ecf8367
ralexrivero <ralexrivero@gmail.com>
2022-09-17 04:01:49 -0300

    dynamic templates using dynamic parameters

--- usingTemplates/usingTemplates/views.py
+++ usingTemplates/usingTemplates/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def simple(request):
     return render(request, 'simple.html', {})
+
+def dynamic(request, name):
+    categories = ['code', 'design', 'marketing']
+    context = {'name': name, 'categories' : categories}
+    return render(request, 'dynamic.html', context)
