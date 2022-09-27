aacd4f551efb8ab32ee8e2a0a8eb903104b7077b
ralexrivero <ralexrivero@gmail.com>
2022-09-26 23:54:46 -0300

    add save() method to save data in the migrated db

--- relations/manytoone/views.py
+++ relations/manytoone/views.py
@@ -4,6 +4,8 @@ from .models import Reporter, Article
 
 def create(request):
     r1 = Reporter(first_name='Ronald', last_name='Rivero', email='ralexrivero@gmail.com')
+    r1.save()
     a1 = Article(headline='Been a Full Stack Developer', pub_date=date.today(), reporter=r1)
+    a1.save()
 
     return render(request, 'manytoone/create.html', {'a1':a1})
