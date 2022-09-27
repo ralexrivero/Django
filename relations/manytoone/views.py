301ed8203e14ead39dc181d0403ba1fc1f99e637
ralexrivero <ralexrivero@gmail.com>
2022-09-27 00:19:51 -0300

    query data

--- relations/manytoone/views.py
+++ relations/manytoone/views.py
@@ -7,5 +7,12 @@ def create(request):
     r1.save()
     a1 = Article(headline='Been a Full Stack Developer', pub_date=date.today(), reporter=r1)
     a1.save()
+    a2 = Article(headline='DevOps Fundation', pub_date=date.today(), reporter=r1)
+    a2.save()
+    a3 = Article(headline='Backend Frameworks', pub_date=date.today(), reporter=r1)
+    a3.save()
 
-    return render(request, 'manytoone/create.html', {'a1':a1})
+    # queries
+    q1 = a1.reporter.first_name
+
+    return render(request, 'manytoone/create.html', {'a1':a1, 'q1':q1})
