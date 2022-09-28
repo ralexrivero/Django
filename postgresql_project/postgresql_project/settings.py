219f8da8345d777f1225945b2b516ec0eab0cbee
ralexrivero <ralexrivero@gmail.com>
2022-09-28 11:26:36 -0300

    default settings, but still not configured with docker compose

--- postgresql_project/postgresql_project/settings.py
+++ postgresql_project/postgresql_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'postgresql_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_sample',
+        'USER': 'postgres',
+        'PASSWORD': 'mysecretpassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
