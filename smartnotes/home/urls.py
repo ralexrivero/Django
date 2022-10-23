2748403c87e06928d5e5e09510ae058951810598
ralexrivero <ralexrivero@gmail.com>
2022-10-23 17:21:47 -0300

    class based views

--- smartnotes/home/urls.py
+++ smartnotes/home/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
-    path('authorized/', views.authorized, name='authorized'),
-
+    path('home', views.HomeView.as_view()),
+    path('authorized/', views.AuthorizedView.as_view()),
 ]
--- smartnotes/home/views.py
+++ smartnotes/home/views.py
@@ -1,13 +1,30 @@
 from logging import logMultiprocessing
+from pipes import Template
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+"""
+Class based views
+"""
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html'
+    extra_context = {'today': datetime.today()}
+
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'
+
+
+"""
+Function based views
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
 
 @login_required(login_url='/admin')
 def authorized(request):
     return render(request, 'home/authorized.html', {})
+"""
