33a5e334d415b57cb79d32425ec451373742e66e
ralexrivero <ralexrivero@gmail.com>
2022-11-11 23:47:08 -0300

    logout, reorder and clean code

--- smartnotes/home/views.py
+++ smartnotes/home/views.py
@@ -6,7 +6,11 @@ from datetime import datetime
 # from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
 
 
 class LoginInterfaceView(LoginView):
@@ -17,18 +21,3 @@ class HomeView(TemplateView):
     template_name = 'home/welcome.html'
     extra_context = {'today': datetime.today()}
 
-
-class AuthorizedView(LoginRequiredMixin, TemplateView):
-    template_name = 'home/authorized.html'
-    login_url = '/admin'
-
-
-"""
-Function based views
-def home(request):
-    return render(request, 'home/welcome.html', {'today': datetime.today()})
-
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html', {})
-"""
