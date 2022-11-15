c7de9219d78fe08b6766f703404cd2f063fcbec7
ralexrivero <ralexrivero@gmail.com>
2022-11-15 03:45:19 -0300

    use ModelForm

---
+++ nandiasgarden-project/pizza/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Pizza, Size
+
+
+class PizzaForm(forms.ModelForm):
+
+    size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
+
+    class Meta:
+        model = Pizza
+        fields = ['topping1', 'topping2', 'size']
+        labels = {'topping1': 'Topping 1', 'topping2': 'Topping 2', 'size': 'Size'}
--- nandiasgarden-project/pizza/views.py
+++ nandiasgarden-project/pizza/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView, CreateView
+from .forms import PizzaForm
 
-
-class HomeView(TemplateView):
+""" class HomeView(TemplateView):
     template_name = 'pizza/home.html'
 
 
-class OrderView(TemplateView):
-    template_name = 'pizza/order.html'
+class OrderView(CreateView):
+    form_class = PizzaForm
+    template_name = 'pizza/order.html' """
 
 
-""" def home(request):
+def home(request):
     return render(request, 'pizza/home.html')
 
 
 def order(request):
-    return render(request, 'pizza/order.html') """
+    form = PizzaForm()
+    return render(request, 'pizza/order.html', {'pizzaform': form})
